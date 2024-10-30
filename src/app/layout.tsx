c5955cf61bff3f309567c533f7a45c0c27ff3d50
bozhovskiy <bozhovski18@gmail.com>
2024-10-30 11:53:38 +0200

    Update HERO + loading test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,39 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
-import React from "react";
+import React, {Suspense} from "react";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
 
 export const metadata: Metadata = {
-  title: "GraphIT",
-  description: "GraphIT description",
+    title: "GraphIT",
+    description: "GraphIT description",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
+    return (
+        <html lang="en">
+        <body>
         <div>
-          <Navbar/>
-          {children}
-          <Footer/>
+            <Navbar/>
+            <Suspense fallback={<div>Loading...</div>}>
+                {children}
+            </Suspense>
+            <Footer/>
         </div>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
 
 export function Loading() {
-  return (
-      <div className="loading">
-       LOADING TEXT
-      </div>
-  );
+    return (
+        <div className="loading">
+            LOADING TEXT
+        </div>
+    );
 }
