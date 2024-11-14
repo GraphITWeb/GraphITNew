a73c45fc1d6f92c1373034b932e2966d038e3c1c
bozhovskiy <bozhovski18@gmail.com>
2024-11-14 15:54:48 +0200

    Our vision animate

--- src/components/who-we-are/OurVision/OurVision.tsx
+++ src/components/who-we-are/OurVision/OurVision.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+'use client'
+import React, { useEffect, useRef, useState } from "react";
 import './OurVision.css';
-import Image, {StaticImageData} from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 interface OurVisionProps {
     style?: React.CSSProperties;
@@ -10,17 +11,53 @@ interface OurVisionProps {
     description: string;
 }
 
-const OurVision: React.FC<OurVisionProps> = ({style, image, svgIcon, title, description}) => {
+const OurVision: React.FC<OurVisionProps> = ({ style, image, svgIcon, title, description }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const contentRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        setIsVisible(true);
+                    } else {
+                        setIsVisible(false);
+                    }
+                });
+            },
+            {
+                threshold: 0.4,
+            }
+        );
+
+        // Якщо реф не порожній, починаємо спостерігати за елементом
+        if (contentRef.current) {
+            observer.observe(contentRef.current);
+        }
+
+        // Очищення при демонтажі компонента
+        return () => {
+            if (contentRef.current) {
+                observer.unobserve(contentRef.current);
+            }
+        };
+    }, []);
+
     return (
-        <div className="our__vision__block" style={{...style}}>
-            <Image src={image} alt="image"/>
-            <div className="our__vision__content">
+        <div className="our__vision__block" style={{ ...style }}>
+            <Image src={image} alt="image" />
+            <div
+                className={`our__vision__content ${isVisible ? 'visible' : ''}`}
+                style={style?.flexDirection=='row'?{ transform: 'translateX(100px)'}:{}}
+                ref={contentRef}
+            >
                 <div className="our__vision__svg">
                     <div className="our__vision__svg__small">
                         {svgIcon}
                     </div>
                 </div>
-                <h3 style={{marginTop: 8}}>{title}</h3>
+                <h3 style={{ marginTop: 8 }}>{title}</h3>
                 <p>{description}</p>
             </div>
         </div>
