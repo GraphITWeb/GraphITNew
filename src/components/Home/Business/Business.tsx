e6ce8b6a8ba703b3fc4ff49deb5e2665b60a4d70
bozhovskiy <bozhovski18@gmail.com>
2024-11-15 22:29:18 +0200

    Companies

--- src/components/Home/Business/Business.tsx
+++ src/components/Home/Business/Business.tsx
@@ -5,10 +5,8 @@ export default function Business() {
     return (
         <div className="business">
             <h2>Take Your Business to the <span style={{color: 'var(--orange)'}}>Next Level</span></h2>
-            <sub style={{color: 'white', marginBottom: 24}}>Contact</sub>
             <Button onClick={() => window.location.href = '/contact-us'} label='Get Started'
                     btnStyle={{padding: '12px 114px'}} btnDivStyle={{fontSize: 18}}/>
-            {/*<Image src={CTAImage} alt='CTA'/>*/}
         </div>
     )
 }
