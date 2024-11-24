6aa28a6414112d4ea7bc37a4b93706064e4b41f0
bozhovskiy <bozhovski18@gmail.com>
2024-11-24 16:10:00 +0200

    Keen

--- src/components/Careers/ToContact/ToContact.tsx
+++ src/components/Careers/ToContact/ToContact.tsx
@@ -4,7 +4,7 @@ import './ToContact.css'
 export default function ToContact() {
     return (
         <div className="ToContact" style={{marginTop:64}}>
-            <h2>Keen To Redefine?<span style={{color: 'var(--orange)'}}>Join Our Team</span></h2>
+            <h2>Keen To Redefine?<br/><span style={{color: 'var(--orange)'}}>Join Our Team</span></h2>
             <Button onClick={() => window.location.href = '/careers'} label='Open Oportunities'
                     btnStyle={{padding: '12px 64px'}} btnDivStyle={{fontSize: 18}}/>
         </div>
