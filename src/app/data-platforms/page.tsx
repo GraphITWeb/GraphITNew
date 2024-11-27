8dabd0cb2cfeec6a27f449d12e0e46ca7b67aefe
bozhovskiy <bozhovski18@gmail.com>
2024-11-27 18:48:08 +0200

    Isopen

--- src/app/data-platforms/page.tsx
+++ src/app/data-platforms/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Main from "@/components/Main/Main";
 import Posts from "@/components/what-we-do/Posts/Posts";
 import OfferingInfo from "@/components/offering/OfferingInfo/OfferingInfo";
-import './nextgen.css'
 import StepSVG from "@/components/offering/stepSvg";
 import Copilots from "@/components/AICopilots/Copilots/Copilots";
 import Business from "@/components/Home/Business/Business";
