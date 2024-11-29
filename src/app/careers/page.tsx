73a4a3de290ca6b24bb5ed723ae9685dfd6da65c
bozhovskiy <bozhovski18@gmail.com>
2024-11-29 13:40:10 +0200

    Careers

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,21 +1,19 @@
 import Main from "@/components/Main/Main";
 import React from "react";
-import Image from "next/image";
-import CareersImage from "@/assets/Careers.webp";
 import './Careers.css'
 import CareersList from "@/components/Careers/CareersList/CareersList";
 import Head from "next/head";
 export default function Careers() {
     return (
         <>
-            <Head>
-                <link rel="preload" href="../../assets/Careers.webp" as="image" />
-            </Head>
             <div className="flex flex-col items-center justify-items-center">
+                <Head>
+                    <link rel="preload" href="../../assets/Careers.webp" as="image" />
+                </Head>
                 <Main subText="Careers" header="Where People Drive Progress"
                       description="Join a team where innovation meets opportunity. Explore a culture that empowers you to grow, lead, and make a lasting impact."
                       />
-                <Image priority={true} src={CareersImage} className="careers__image" alt="Careers"/>
+                <img src="../../assets/Careers.webp" className="careers__image" alt="Careers"/>
                 <CareersList name='Open Positions'/>
             </div>
         </>
