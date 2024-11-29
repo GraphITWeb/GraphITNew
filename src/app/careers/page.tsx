e344253b10ba4bdbedf4af68322dfd2b38def5ba
bozhovskiy <bozhovski18@gmail.com>
2024-11-29 13:44:35 +0200

    Careers

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,9 +1,11 @@
 import Main from "@/components/Main/Main";
 import React from "react";
+import CareersImage from "@/assets/Careers.webp";
 import './Careers.css'
 import CareersList from "@/components/Careers/CareersList/CareersList";
 import Head from "next/head";
 export default function Careers() {
+
     return (
         <>
             <div className="flex flex-col items-center justify-items-center">
@@ -13,7 +15,10 @@ export default function Careers() {
                 <Main subText="Careers" header="Where People Drive Progress"
                       description="Join a team where innovation meets opportunity. Explore a culture that empowers you to grow, lead, and make a lasting impact."
                       />
-                <img src="../../assets/Careers.webp" className="careers__image" alt="Careers"/>
+                {
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-expect-error
+                }<img src={CareersImage} className="careers__image" alt="Careers"/>
                 <CareersList name='Open Positions'/>
             </div>
         </>
