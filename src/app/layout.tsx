91d546c2be33651d4b8506dd16d763ad7de12306
bozhovskiy <bozhovski18@gmail.com>
2024-11-30 17:48:49 +0200

    Careers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./case-study.css";
 import React from "react";
 import Navbar from "@/components/UI/Navbar/Navbar";
 import Footer from "@/components/UI/Footer/Footer";
-
+import { Analytics } from "@vercel/analytics/react"
 export const metadata: Metadata = {
     title: "Graphit",
     description: "Graphit is a London-based technology-consulting firm that operates on the intersection of data, AI and business strategy.",
@@ -22,6 +22,7 @@ export default function RootLayout({
         <html lang="en">
         <body>
         <div>
+            <Analytics/>
             <Navbar/>
             {children}
             <Footer/>
