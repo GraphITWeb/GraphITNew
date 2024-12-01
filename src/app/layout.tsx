f4961f7e0ec1cf7372d11b93862aa1d7634d32d1
bozhovskiy <bozhovski18@gmail.com>
2024-12-01 21:27:10 +0200

    Update css logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import Navbar from "@/components/UI/Navbar/Navbar";
 import Footer from "@/components/UI/Footer/Footer";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Link from "next/link";
+import Head from "next/head";
 export const metadata: Metadata = {
     title: "Graphit",
     description: "Graphit is a London-based technology-consulting firm that operates on the intersection of data, AI and business strategy.",
@@ -22,6 +24,16 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+        <Head>
+            <Link
+                href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,400..700;1,400..700&display=swap"
+                rel="stylesheet"
+            />
+            <Link
+                href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,400..700;1,100..700&display=swap"
+                rel="stylesheet"
+            />
+        </Head>
         <body>
         <div>
             <Analytics/>
