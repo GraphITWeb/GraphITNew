399f5b6ae58eab5cee36fab84e5251c1edf31524
bozhovskiy <bozhovski18@gmail.com>
2024-12-02 12:07:30 +0200

    Fonts update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,23 @@ import './Navbar.css'
 import React from "react";
 import Navbar from "@/components/UI/Navbar/Navbar";
 import Footer from "@/components/UI/Footer/Footer";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import {Analytics} from "@vercel/analytics/react"
+import {SpeedInsights} from "@vercel/speed-insights/next"
+import {Figtree, Hanken_Grotesk} from 'next/font/google';
+
+const figtree = Figtree({
+    weight: ['400', '500', '600', '700'],
+    subsets: ["latin","latin-ext"],
+    display: 'swap',
+    variable:'--figtree',
+
+});
+const hanken = Hanken_Grotesk({
+    weight: ['400', '500', '600', '700']
+    , subsets: ["latin","latin-ext","cyrillic-ext","vietnamese"],
+    display: 'swap',
+    variable:'--hanken-grotesk'
+});
 export const metadata: Metadata = {
     title: "Graphit",
     description: "Graphit is a London-based technology-consulting firm that operates on the intersection of data, AI and business strategy.",
@@ -21,7 +36,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={`${figtree.variable} ${hanken.variable}`}>
         <body>
         <div>
             <Analytics/>
