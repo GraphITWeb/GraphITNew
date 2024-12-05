11f313713ecb4494922ffe8f7b8f914a74bc38a2
bozhovskiy <bozhovski18@gmail.com>
2024-12-05 16:08:22 +0200

    Fix

--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -38,6 +38,10 @@ const Navbar = () => {
             document.removeEventListener('mousedown', handleClickOutsideDo);
         };
     }, []);
+    useEffect(()=>{
+       setIsOpen(false)
+        setIsOpenMobile(false)
+    },[])
 
     return (
         <>
