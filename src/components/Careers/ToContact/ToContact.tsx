d62399d6064175aaf782f0459bf2d2c65de60fd4
bozhovskiy <bozhovski18@gmail.com>
2024-12-08 12:22:50 +0200

    Update

--- src/components/Careers/ToContact/ToContact.tsx
+++ src/components/Careers/ToContact/ToContact.tsx
@@ -4,7 +4,7 @@ export default function ToContact() {
     return (
         <div className="ToContact" style={{marginTop:64}}>
             <h2>Keen To Redefine?<br/><span style={{color: 'var(--orange)'}}>Join Our Team</span></h2>
-            <Button href = '/careers' label='Open Oportunities'
+            <Button href = '/careers' label='See open opportunities'
                     btnStyle={{padding: '12px 64px'}} btnDivStyle={{fontSize: 18}}/>
         </div>
     )
