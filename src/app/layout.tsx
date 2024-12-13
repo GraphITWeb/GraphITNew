2aa106718c8e4c9943a50b60b415a0feed3c7d77
bozhovskiy <bozhovski18@gmail.com>
2024-12-13 12:28:59 +0200

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     title: "Graphit",
     description: "Graphit is a London-based technology-consulting firm that operates on the intersection of data, AI and business strategy.",
     icons: {
-        icon: './favicon.png',
+        icon: './favicon.ico',
     },
 };
 
