d03cef1da1ce2e363c961d978c93a0b267840308
bozhovskiy <bozhovski18@gmail.com>
2024-12-13 12:18:44 +0200

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import Footer from "@/components/UI/Footer/Footer";
 import {Analytics} from "@vercel/analytics/react"
 import {SpeedInsights} from "@vercel/speed-insights/next"
 import {Figtree, Hanken_Grotesk} from 'next/font/google';
-import Head from "next/head";
 
 const figtree = Figtree({
     weight: ['400', '500', '600', '700'],
